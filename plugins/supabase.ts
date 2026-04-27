6495c86b61b6311df42fbcb8fc30662304788a05
machinavicente <mangotoday1304@gmail.com>
2026-04-26 21:17:54 -0400

    Actualización: galería configurada con el nuevo bucket

--- plugins/supabase.ts
+++ plugins/supabase.ts
@@ -2,7 +2,6 @@ import { createClient } from '@supabase/supabase-js'
 
 export default defineNuxtPlugin(nuxtApp => {
   const config = useRuntimeConfig()
-<<<<<<< HEAD
 
   const url = config.public.supabaseUrl
   const key = config.public.supabaseKey
@@ -17,9 +16,3 @@ export default defineNuxtPlugin(nuxtApp => {
   // El nombre '$supabase' es el que usas en tus componentes (login.vue, etc.)
   nuxtApp.provide('supabase', supabase)
 })
-=======
-  const supabase = createClient(config.public.supabaseUrl, config.public.supabaseKey)
-
-  nuxtApp.provide('supabase', supabase)
-})
->>>>>>> 65f12d4795b9284c5cf986ae2453462f7b064921
